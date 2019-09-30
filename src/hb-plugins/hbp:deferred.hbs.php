885535523439b00fa4bf8bd0db5fb423f9fe5d03
Don Myers <donmyers@me.com>
2019-09-30 10:13:49 -0400

    add some deferred stuff

--- src/hb-plugins/hbp:deferred.hbs.php
+++ src/hb-plugins/hbp:deferred.hbs.php
@@ -1,24 +1,9 @@
 <?php
 
 $helpers['hbp:deferred'] = function($options) {
-	if (isset($options['hash']['key'])) {
-		$key = $options['hash']['key'];
+	if (isset($options['hash']['id'])) {
+		return new \LightnCandy\SafeString('<i id="'.$options['hash']['id'].'"></i>');
 	} else {
-		$key = $options['data']['root']['deferred'].uniqid('_s_',true);
-
-		$s = new stdClass;
-
-		$s->fn = $options['fn']();
-		$s->_this = $options['_this'];
-
-		file_put_contents(__ROOT__.'/var/cache/'.$key.'.txt',$key.chr(0).serialize($s));
-
-		/* render the section -- !todo can we process this later??? */
-		$html = $options['fn']($options['_this']);
-
-		/* save it */
-		file_put_contents(__ROOT__.'/var/cache/'.$key.'.deferred',$key.chr(0).$html);
+		return new \LightnCandy\SafeString(ci('output')->injector(true));
 	}
-
-	return new \LightnCandy\SafeString('<i id="'.$key.'"></i>');
 };
